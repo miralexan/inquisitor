907373a06bb437ba00d369e9bd352f18b4350a79
miralexan <e.trovillion@gmail.com>
2017-03-16 22:57:11 -0500

    Refactor Actions into Functions
    Change the Action type so that it is an alias for Config => Unit instead
    of a seperate class.

--- src/main/scala/inquisitor/Actions.scala
+++ src/main/scala/inquisitor/Actions.scala
@@ -14,110 +14,69 @@ import scala.concurrent.duration._
 import scala.concurrent.Await
 import com.lucidchart.open.xtract.ParseError
 import java.net.URI
+import scala.concurrent.Future
 
-object Actions {
+object Actions extends StrictLogging {
   import Configuration._
+  import scala.concurrent.ExecutionContext.Implicits.global
 
-  trait Action {
-    def apply(config: Config): Unit
+  type Action = Config => Unit
+
+  val Create: Action = config => {
+    val setupAction = (testCases.schema ++ testResults.schema).create
+
+    val testResultStream = Files.getDirectoryStream(Seq(config.evidence.toPath())).map(_.toFile()).collect(testResultReader).flatten
+    await(execute(setupAction >> insertTestResultsForTestCases(testResultStream))(config)).fold(logger.info("No results could be added."))(x => logger.info(s"Added $x test results"))
   }
 
-  object Create extends Action with StrictLogging {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
-    override def apply(config: Config): Unit = {
-      val setupAction = (testCases.schema ++ testResults.schema).create
-
-      val testResultReader: PartialFunction[File, Seq[TestResult]] = {
-        case x if x.getName.endsWith(".xml") => XmlReader.of[TestSuite].read(XML.loadFile(x)).fold(logParseError(x)_)(logParseSuccess(x)_)
-      }
-
-      val testResultStream = Files.getDirectoryStream(Seq(config.evidence.toPath())).map(_.toFile()).collect(testResultReader).flatten
-
-      val db = Database.forURL(new URI("jdbc:sqlite", config.outFile.toURI().getSchemeSpecificPart, null).toString(), driver = "org.sqlite.JDBC")
-      val databaseExecution = db.run(setupAction >> insertTestResultsForTestCases(testResultStream))
-      databaseExecution.onComplete(_ => db.close())
-      Await.result(databaseExecution, 30 seconds).fold(logger.info("No results could be added."))(x => logger.info(s"Added $x test results"))
-    }
-
-    def getTestCaseMap(rows: Traversable[(Int, String, String)]) =
-      if (rows.isEmpty) Map[TestCase, Int]()
-      else rows.map { case (id, clazz, name) => Map((TestCase(clazz, name), id)) }.reduce(_ ++ _)
-
-    def getExistingTestCaseIds(tcs: Seq[TestCase]) = getTestCaseIds(tcs.toSet).result.map(getTestCaseMap(_))
-
-    def getOrInsertTestCases(tcs: Seq[TestCase]) = getExistingTestCaseIds(tcs).flatMap { f =>
-      insertTestCases(tcs.toSet.filter(!f.contains(_))).map(g => f ++ getTestCaseMap(g))
-    }
-
-    def insertTestResultsForTestCases(trs: Seq[TestResult]) = getOrInsertTestCases(trs.map(_.testCase)).flatMap { map =>
-      insertTestResults(trs, map.compose(_.testCase))
-    }
-
-    def logParseSuccess(file: File)(s: TestSuite) = {
-      logger.info(s"Successfully parsed file $file")
-      s.testResults
-    }
-
-    def logParseError(file: File)(pes: Seq[ParseError]) = {
-      logger.warn(s"Could not parse file $file")
-      logger.debug(pes.toString())
-      Seq.empty[TestResult]
-    }
+  val Add: Action = config => {
+    val testResultStream = Files.getDirectoryStream(Seq(config.evidence.toPath())).map(_.toFile()).collect(testResultReader).flatten
+    await(execute(insertTestResultsForTestCases(testResultStream))(config)).fold(logger.info("No results could be added."))(x => logger.info(s"Added $x test results"))
   }
 
-  object Add extends Action with StrictLogging {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
-    override def apply(config: Config): Unit = {
-
-      val testResultReader: PartialFunction[File, Seq[TestResult]] = {
-        case x if x.getName.endsWith(".xml") => XmlReader.of[TestSuite].read(XML.loadFile(x)).fold(logParseError(x)_)(logParseSuccess(x)_)
-      }
-
-      val testResultStream = Files.getDirectoryStream(Seq(config.evidence.toPath())).map(_.toFile()).collect(testResultReader).flatten
-
-      val db = Database.forURL(new URI("jdbc:sqlite", config.outFile.toURI().getSchemeSpecificPart, null).toString(), driver = "org.sqlite.JDBC")
-      val databaseExecution = db.run(insertTestResultsForTestCases(testResultStream))
-      databaseExecution.onComplete(_ => db.close())
-      Await.result(databaseExecution, 30 seconds).fold(logger.info("No results could be added."))(x => logger.info(s"Added $x test results"))
-    }
-
-    def getTestCaseMap(rows: Traversable[(Int, String, String)]) =
-      if (rows.isEmpty) Map[TestCase, Int]()
-      else rows.map { case (id, clazz, name) => Map((TestCase(clazz, name), id)) }.reduce(_ ++ _)
-
-    def getExistingTestCaseIds(tcs: Seq[TestCase]) = getTestCaseIds(tcs.toSet).result.map(getTestCaseMap(_))
-
-    def getOrInsertTestCases(tcs: Seq[TestCase]) = getExistingTestCaseIds(tcs).flatMap { f =>
-      insertTestCases(tcs.toSet.filter(!f.contains(_))).map(g => f ++ getTestCaseMap(g))
-    }
-
-    def insertTestResultsForTestCases(trs: Seq[TestResult]) = getOrInsertTestCases(trs.map(_.testCase)).flatMap { map =>
-      insertTestResults(trs, map.compose(_.testCase))
-    }
-
-    def logParseSuccess(file: File)(s: TestSuite) = {
-      logger.info(s"Successfully parsed file $file")
-      s.testResults
-    }
-
-    def logParseError(file: File)(pes: Seq[ParseError]) = {
-      logger.warn(s"Could not parse file $file")
-      logger.debug(pes.toString())
-      Seq.empty[TestResult]
-    }
+  val Validate: Action = c => {
+    val foo = getTestResultsInStatus().map(tcs => if (tcs.isEmpty) logger.info("No unaccounted errors!") else tcs.foreach(tc => logger.warn(s"No successes recorded for ${tc.className}#${tc.name}")))
+    await(execute(foo)(c))
   }
 
-  object Validate extends Action with StrictLogging {
-    import scala.concurrent.ExecutionContext.Implicits.global
+  val testResultReader: PartialFunction[File, Seq[TestResult]] = {
+    case x if x.getName.endsWith(".xml") => XmlReader.of[TestSuite].read(XML.loadFile(x)).fold(logParseError(x)_)(logParseSuccess(x)_)
+  }
 
-    override def apply(config: Config) = {
-      val db = Database.forURL(new URI("jdbc:sqlite", config.outFile.toURI().getSchemeSpecificPart, null).toString(), driver = "org.sqlite.JDBC")
-      val f = db.run(getTestResultsInStatus()).map(tcs => if (tcs.isEmpty) logger.info("No unaccounted errors!") else tcs.foreach(tc => logger.warn(s"No successes recorded for ${tc.className}#${tc.name}")))
-      f.onComplete(_ => db.close)
-      Await.result(f, 30 seconds)
-    }
+  def await[T](future: Future[T]) = Await.result(future, 30 seconds)
+
+  def execute[T](dbio: DBIOAction[T, NoStream, Effect.All])(config: Config) = {
+    val db = Database.forURL(buildSqliteUrl(config.outFile), driver = "org.sqlite.JDBC")
+    val databaseExecution = db.run(dbio)
+    databaseExecution.onComplete(_ => db.close())
+    databaseExecution
+  }
+
+  def buildSqliteUrl(f: File) = new URI("jdbc:sqlite", f.toString(), null).toString()
+
+  def getTestCaseMap(rows: Traversable[(Int, String, String)]) =
+    if (rows.isEmpty) Map[TestCase, Int]()
+    else rows.map { case (id, clazz, name) => Map((TestCase(clazz, name), id)) }.reduce(_ ++ _)
+
+  def getExistingTestCaseIds(tcs: Seq[TestCase]) = getTestCaseIds(tcs.toSet).result.map(getTestCaseMap(_))
+
+  def getOrInsertTestCases(tcs: Seq[TestCase]) = getExistingTestCaseIds(tcs).flatMap { f =>
+    insertTestCases(tcs.toSet.filter(!f.contains(_))).map(g => f ++ getTestCaseMap(g))
+  }
+
+  def insertTestResultsForTestCases(trs: Seq[TestResult]) = getOrInsertTestCases(trs.map(_.testCase)).flatMap { map =>
+    insertTestResults(trs, map.compose(_.testCase))
+  }
+
+  def logParseSuccess(file: File)(s: TestSuite) = {
+    logger.info(s"Successfully parsed file $file")
+    s.testResults
+  }
+
+  def logParseError(file: File)(pes: Seq[ParseError]) = {
+    logger.warn(s"Could not parse file $file")
+    logger.debug(pes.toString())
+    Seq.empty[TestResult]
   }
 
 }
